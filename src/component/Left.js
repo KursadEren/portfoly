0b0c539422f4a81eef76bac713e8012e815b704c
KursadEren <k.erenmaden1@outlook.com>
2023-09-07 21:40:43 +0300

    düzenleme

--- src/component/Left.js
+++ src/component/Left.js
@@ -2,15 +2,13 @@ import React from 'react';
 
 export default function Left() {
   return (
-    <div className='Left-container'>
-       <div className='left-1'>
-           <div className='left-img'>
-           <a><img  src="./kursaderen.jpg" alt=''/></a> 
-           </div>
-           
-       </div>
+     <div className='Left-container'>
+         <div className='left-1'>
+             <div className='left-img'>
+               <a><img  src="./kursaderen.jpg" alt=''/></a> 
+             </div>
+         </div>
        <style jsx>{`
-       
        .Left-container{
         display:flex;
         flex-direction:column;
@@ -19,10 +17,9 @@ export default function Left() {
         height:100%;
         justify-content:space-around;
         align-items:center;
-        
        }
+
        .left-1{
-        
         background:#000000;
         margin-top:20px;
         display:flex;
@@ -32,11 +29,13 @@ export default function Left() {
         justify-content:center;
         align-items:center;
        }
+
        .left-img img{
         display:flex;
         width:285px;
         height:285px;
        }
+
        .left-img{
         display:flex;
         background:white;
@@ -46,6 +45,7 @@ export default function Left() {
         height:285px;
         border : 1px solid #000000;
        }
+
        .left-info{
         width:300px;
         height:300px;
@@ -53,31 +53,27 @@ export default function Left() {
         padding:25px;
         color:#000000;
        }
+
        .github-link {
         text-decoration: underline;
         color:#7777;
-        
        }
+
        .github-link:hover{
         color:#000000;
        }
        `}
-       
        </style>
-
-       <div className='left-info'>
-              <ul>
-                <li>
-                 
-                 <a> Github: <a className='github-link' href='https://github.com/KursadEren'> KÜRŞAD EREN MADEN</a></a><br/><br/>
-                 <a> Linkedin: <a className='github-link' href=' https://www.linkedin.com/in/kursad-maden-385715259/' >KÜRŞAD EREN MADEN</a></a><br/><br/>
-                 <a> iletişim: +905539777143 </a>  <br/><br/>
-                 <a> Email: k.erenmaden1@outlook.com </a>
-                
-                </li>
-              </ul>
-
-           </div>
-    </div>
+         <div className='left-info'>
+               <ul>
+                 <li>
+                   <a> Github: <a className='github-link' href='https://github.com/KursadEren'> KÜRŞAD EREN MADEN</a></a><br/><br/>
+                   <a> Linkedin: <a className='github-link' href=' https://www.linkedin.com/in/kursad-maden-385715259/' >KÜRŞAD EREN MADEN</a></a><br/><br/>
+                   <a> iletişim: +905539777143 </a>  <br/><br/>
+                   <a> Email: k.erenmaden1@outlook.com </a>
+                 </li>
+               </ul>
+         </div>
+     </div>
   )
 }
